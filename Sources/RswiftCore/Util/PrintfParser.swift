82d1dd3386622f4127f36241dd3a7be3d470e554
masakih <masakih@users.sourceforge.jp>
2021-07-14 20:24:19 +0900

    各識別子を変更

--- Sources/RswiftCore/Util/PrintfParser.swift
+++ Sources/RswiftCore/Util/PrintfParser.swift
@@ -8,81 +8,83 @@
 import FootlessParser
 
 enum LocalizedStringParser {
-  
+
+  // parse String Format Specifiers and the IEEE printf specification.
+  //
+  // %[parameter][flags][width][.precision][length]type
+
+  typealias ParsedType = Parser<Character, Optional<FormatPart>>
+
+  static func formatPartSpec<A>(_ output: FormatSpecifier?) -> (A) -> FormatPart? {
+    { _ in output.map { .spec($0) } }
+  }
+
+  static func filtered(parts: [FormatPart?]) -> [FormatPart] {
+    parts.compactMap { $0 }
+  }
+
   static let percent = char("%")
-  
-  static let formatParam = oneOrMore(digit) *> string("$")
-  static let formatFlag = string("-") <|> string("+") <|> string(" ") <|> string("0") <|> string("'") <|> string("#")
-  static let formatWidth = oneOrMore(digit)
-  static let formatPrecision = string(".") *> zeroOrMore(digit)
-  static let fotmatLength = string("hh") <|> string("h") <|>
-    string("l") <|> string("ll") <|> string("L") <|>
+
+  static let parameter = oneOrMore(digit) *> string("$")
+  static let flags = string("-") <|> string("+") <|> string(" ") <|> string("0") <|> string("'") <|> string("#")
+  static let width = oneOrMore(digit)
+  static let precision = string(".") *> zeroOrMore(digit)
+  static let length = string("hh") <|> string("h") <|>
     string("ll") <|> string("l") <|> string("L") <|>
     string("q") <|>
     string("z") <|>
     string("j") <|>
     string("t")
-  
-  static let formatOptions = formatParam <|>
-    formatFlag <|>
-    formatWidth <|>
-    formatPrecision <|>
-    fotmatLength
-  
-  static let formatObjectP = char("@")
-  static let formatDoubleP = char("a") <|> char("A") <|>
+
+  static let objectTypePerser = char("@")
+  static let doubleTypePerser = char("a") <|> char("A") <|>
     char("e") <|> char("E") <|>
     char("f") <|> char("F") <|>
     char("g") <|> char("G")
-  static let formatIntP = char("d") <|> char("i")
-  static let formatUIntP = char("o") <|> char("O") <|> char("u") <|> char("x") <|> char("X")
-  static let formatCharacterP = char("c") <|> char("C")
-  static let formatStringPointerP = char("s") <|> char("S")
-  static let formatVoidPointerP = char("p")
-  
-  static func formatPartSpec<A>(_ output: FormatSpecifier?) -> (A) -> FormatPart? {
-    { _ in output.map { .spec($0) } }
-  }
-  
-  typealias ParsedType = Parser<Character, Optional<FormatPart>>
-  static let percentT: ParsedType = formatPartSpec(nil) <^> percent
-  static let formatObject: ParsedType = formatPartSpec(.object) <^> formatObjectP
-  static let formatDouble: ParsedType = formatPartSpec(.double) <^> formatDoubleP
-  static let formatInt: ParsedType = formatPartSpec(.int) <^> formatIntP
-  static let formatUInt: ParsedType = formatPartSpec(.uInt) <^> formatUIntP
-  static let formatCharacter: ParsedType = formatPartSpec(.character) <^> formatCharacterP
-  static let formatStringPointer: ParsedType = formatPartSpec(.cStringPointer) <^> formatStringPointerP
-  static let formatVoidPointer: ParsedType = formatPartSpec(.voidPointer) <^> formatVoidPointerP
-  
-  static let formatType = percentT <|>
-    formatObject <|>
-    formatDouble <|>
-    formatInt <|>
-    formatUInt <|>
-    formatCharacter <|>
-    formatStringPointer <|>
-    formatVoidPointer
-  
+  static let intTypePerser = char("d") <|> char("i")
+  static let uIntTypePerser = char("o") <|> char("O") <|> char("u") <|> char("x") <|> char("X")
+  static let characterTypePerser = char("c") <|> char("C")
+  static let cStringPointerTypePerser = char("s") <|> char("S")
+  static let voidPointerTypePerser = char("p")
+
+  // exchange to Optional<FormatSpecifier>
+  static let percentType: ParsedType = formatPartSpec(nil) <^> percent
+  static let objectType: ParsedType = formatPartSpec(.object) <^> objectTypePerser
+  static let doubleType: ParsedType = formatPartSpec(.double) <^> doubleTypePerser
+  static let intType: ParsedType = formatPartSpec(.int) <^> intTypePerser
+  static let uIntType: ParsedType = formatPartSpec(.uInt) <^> uIntTypePerser
+  static let characterType: ParsedType = formatPartSpec(.character) <^> characterTypePerser
+  static let cStringPointerType: ParsedType = formatPartSpec(.cStringPointer) <^> cStringPointerTypePerser
+  static let voidPointerType: ParsedType = formatPartSpec(.voidPointer) <^> voidPointerTypePerser
+
+  static let type = percentType <|>
+    objectType <|>
+    doubleType <|>
+    intType <|>
+    uIntType <|>
+    characterType <|>
+    cStringPointerType <|>
+    voidPointerType
+
   static let placeHolder = percent *>
-    zeroOrMore(formatParam) *>
-    zeroOrMore(formatFlag) *>
-    zeroOrMore(formatWidth) *>
-    zeroOrMore(formatPrecision) *>
-    zeroOrMore(fotmatLength) *>
-    zeroOrMore(formatOptions) *>
-    formatType
-  
+    count(0...1, parameter) *>
+    count(0...1, flags) *>
+    count(0...1, width) *>
+    count(0...1, precision) *>
+    count(0...1, length) *>
+    type
+
+  // none placeholder text
   static let text: ParsedType = formatPartSpec(nil) <^> oneOrMore(not("%"))
-  
+
+  // stringsdict variable
+  // %#@named@
   static let keyName = oneOrMore(oneOf("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"))
   static let referencePlaceHolder: ParsedType = FormatPart.reference <^> ( string("%#@") *> keyName <* char("@") )
-  
+
+  // parse full strings
   static let printfFormatted = zeroOrMore(text <|> referencePlaceHolder <|> placeHolder)
-  
-  static func filtered(parts: [FormatPart?]) -> [FormatPart] {
-    parts.compactMap { $0 }
-  }
-  
-  
+
+  // exclude Optional<FormatSpecifier>.none
   static let formatParts = filtered <^> printfFormatted
 }
